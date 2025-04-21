eb1a670ac699cb6bf5288d6d5b790916d34bc04c
brinoceros484 <137417915+brinoceros484@users.noreply.github.com>
2025-04-21 22:13:56 +0000

    adding items at bottom

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-
-st.title("Grocery List App")
+col_1header, col2_header, col3_header = st.columns(3)
+col2_header.title("Grocery List App")
 st.write(
     "Click on a recipe button for information on the given recipe"
 )
@@ -10,6 +10,9 @@ st.write(
 st.write(
     "Are there any Chefs you want to filter for?"
 )
+
+if 'grocery_list' not in st.session_state:
+    st.session_state.grocery_list = []
 
 file_url = 'https://raw.githubusercontent.com/brinoceros484/grocery_list/refs/heads/main/RecipeGeneratorSheet1.csv'
 
@@ -26,32 +29,33 @@ for i, chef in enumerate(unique_chefs):
 
 st.divider()
 
-
 for meal in unique_meals:
     row = df[df['Meal Name'] == meal].iloc[0]
     img_path = row['Dish Picture']
+    ingredients = df[df['Meal Name'] == meal]['Ingrediants'].tolist()
+    cookbook = row['Cookbook']
 
-    main_col1, main_col2 = st.columns(2)
+    with st.container():
+        col1, col2 = st.columns([1, 3])
+        with col1:
+            st.image(img_path, use_container_width=True)
+        with col2:
+            expand = st.expander(meal)  # CHANGED from button to expander
+            with expand:
+                st.write(f"{meal} is in **{cookbook}** and the ingredients are:")
+                st.table(ingredients)
+                
+                # NEW: Checkbox to add ingredients
+                if st.checkbox(f"Add {meal} to grocery list?", key=meal):
+                    for ingredient in ingredients:
+                        if ingredient not in st.session_state.grocery_list:
+                            st.session_state.grocery_list.append(ingredient)
 
-    with main_col1:
-        with st.container():
-            col1, col2 = st.columns([1, 3])
-            with col1:
-                st.image(img_path, use_container_width=True)
-            with col2:
-                if st.button(meal):
-                    st.session_state[f"{meal}_clicked"] = True
-
-                if st.session_state.get(f"{meal}_clicked", False):
-                    ingredients = df[df['Meal Name'] == meal]['Ingrediants']
-                    cookbook = row['Cookbook']
-                    st.write(f"{meal} is in **{cookbook}** and the ingredients are:")
-                    st.table(ingredients)
-                    grocery_list = st.checkbox("Add to grocery list?", key=f"grocery_{meal}")
-                    if grocery_list:
-                        chosen_ingredients = ingredients
-
-    with main_col2:
-            st.write(chosen_ingredients)
-
-
+# NEW: Show grocery list
+st.divider()
+st.subheader("Your Grocery List:")
+if st.session_state.grocery_list:
+    #st.write(st.session_state.grocery_list)
+    st.table(st.session_state.grocery_list)
+else:
+    st.write("No items yet. Select recipes above!")
